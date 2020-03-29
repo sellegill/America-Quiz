43e2a51be6be3eb991f4861bb55b895a23963e34
Selle Gill <savannahsellegill@Savannahs-Air.zyxel.com>
2020-03-29 13:06:21 -0500

    commit

---
+++ datastore.js
@@ -0,0 +1,106 @@
+"use strict"
+
+const STORE = [
+  {
+  question: "What territory did the United States buy from France in 1803?",
+  answers: [
+    "The Puerto Rico Territory",
+    "The Hawaiian Territory",
+    "The Louisiana Territory",
+    "All of the above",
+
+  ],
+correctAnswer:"The Louisiana Territory",
+ icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+alt: "American Flag"
+
+  },
+
+  {
+    question: "Name one war fought by the United States in the 1800s.",
+
+    answers: [
+    "King George’s War",
+    "Civil War",
+    "French and Indian War",
+    "American Revolution",
+    ],
+  correctAnswer: "Civil War",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+alt: "American Flag"
+  },
+
+  {
+    question: "Who wrote the Declaration of Independence?",
+
+    answers: [
+    "Benjamin Franklin",
+    "Thomas Jefferson",
+    "George Washington",
+    "John Adams", 
+    ],
+  correctAnswer: "Thomas Jefferson wrote the Declaration of Independence in 1776 after John Adams appointed him as the composer of the document.",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+alt: "American Flag"
+  },
+
+  {
+    question:"How many U.S. Senators are there?",
+
+    answers: [
+      "435",
+      "100",
+      "52",
+      "86",
+      
+    ],
+  correctAnswer: "The U.S. Senate provides equal representation for all 50 states, and at 2 senators per state, there are 100 senators total.",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+  alt: "American Flag"
+  },
+
+  {
+    question: "Which state was the first to secede from the Union before the start of the Civil War?",
+
+    answers: [
+      "Louisiana", 
+      "Minnesota", 
+      "South Carolina",
+      "Texas", 
+    ],
+  correctAnswer: "South Carolina",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+  alt: "American Flag"
+  
+  },
+
+  {
+    question: "What amendment ended prohibition?",
+
+    answers: [
+    "13th Amendment", 
+    "21st Amendment", 
+    "18th Amendment",
+    "14th Amendment",
+    ],
+  correctAnswer: "21st Amendment",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+  alt: "American Flag"
+  },
+
+  {
+    question:"Which president served three terms in office?", 
+
+    answers: [
+   "Harry Truman",
+   "Grover Cleveland",
+   "Franklin Rosevelt",
+   "Woodrow Wilson",  
+    ],
+  correctAnswer: "Franklin Roosevelt",
+  icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Flag_of_the_United_States_%281912-1959%29.svg/1280px-Flag_of_the_United_States_%281912-1959%29.svg.png",
+  alt: "American Flag"
+  },
+
+
+];
